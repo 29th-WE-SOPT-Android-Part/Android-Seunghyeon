9a52822ae290c241baab860b934e96740b9a6ceb
Seunghyeon Han <81508084+hansh0101@users.noreply.github.com>
2021-10-07 00:34:38 +0900

    #1 [FEAT] : SEMINAR_1

---
+++ app/src/main/java/co/kr/soptandroidseminar/SignUpActivity.kt
@@ -0,0 +1,29 @@
+package co.kr.soptandroidseminar
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import co.kr.soptandroidseminar.databinding.ActivitySignUpBinding
+
+class SignUpActivity : AppCompatActivity() {
+    private lateinit var binding: ActivitySignUpBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivitySignUpBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnSignupOk.setOnClickListener {
+            clickSignUp()
+        }
+    }
+
+    private fun clickSignUp() {
+        if(!binding.etSignupName.text.isNullOrBlank() && !binding.etSignupId.text.isNullOrBlank() && !binding.etSignupPw.text.isNullOrBlank()) {
+            Toast.makeText(this, "회원가입이 완료되었습니다.", Toast.LENGTH_SHORT).show()
+            finish()
+        } else {
+            Toast.makeText(this, "이름/ID/PW를 확인해주세요.", Toast.LENGTH_SHORT).show()
+        }
+    }
+}
