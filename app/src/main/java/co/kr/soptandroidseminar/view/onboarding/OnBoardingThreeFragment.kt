ab65c944a7f4816b349790c1f21a0c3fb19fdf34
Seunghyeon Han <81508084+hansh0101@users.noreply.github.com>
2021-12-16 17:15:04 +0900

    #10 [REFACTOR] : startActivity -> navigate 수정

--- app/src/main/java/co/kr/soptandroidseminar/view/onboarding/OnBoardingThreeFragment.kt
+++ app/src/main/java/co/kr/soptandroidseminar/view/onboarding/OnBoardingThreeFragment.kt
@@ -6,6 +6,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import co.kr.soptandroidseminar.R
 import co.kr.soptandroidseminar.databinding.FragmentOnBoardingThreeBinding
 import co.kr.soptandroidseminar.view.signin.SignInActivity
@@ -29,8 +30,7 @@ class OnBoardingThreeFragment : Fragment() {
 
     private fun skipOnBoarding() {
         binding.btnOnboardingThree.setOnClickListener {
-            val intent = Intent(requireContext(), SignInActivity::class.java)
-            startActivity(intent)
+            findNavController().navigate(R.id.action_frg_onboarding_three_to_activity_signin)
             (activity as OnBoardingActivity).finish()
         }
     }
